362df7e0c233d9b6d63afc913ce4dd7766174474
Po Co <polniolino@gmail.com>
2023-08-29 14:09:21 +0200

    Support multi-proxying

--- config/config.exs
+++ config/config.exs
@@ -2,6 +2,7 @@ import Config
 
 config :proxy,
   upstreams: [
-    [4040, "localhost:4000", 2, :proxy],
+    [4040, "localhost:4000", 2, :proxy1],
+    [4041, "localhost:4001", 2, :proxy2],
   ]
 
--- lib/proxy.ex
+++ lib/proxy.ex
@@ -10,10 +10,13 @@ defmodule Proxy do
     upstreams = Application.get_env(:proxy, :upstreams, [])
 
     processes = upstreams
-      |> Enum.map(fn upstream -> [
-        {Proxy.TCP, parse_upstream(upstream, :tcp)},
-        {Proxy.UDP, parse_upstream(upstream, :udp)}
-      ] end)
+      |> Enum.map(fn upstream ->
+        id_tcp = parse_name(upstream, :tcp)
+        id_udp = parse_name(upstream, :udp)
+        [
+          Supervisor.child_spec({Proxy.TCP, parse_upstream(upstream, :tcp)}, id: id_tcp),
+          Supervisor.child_spec({Proxy.UDP, parse_upstream(upstream, :udp)}, id: id_udp)
+        ] end)
       |> Enum.flat_map(& &1)
     opts = [strategy: :one_for_one, name: Proxy.Supervisor]
     Supervisor.start_link(processes, opts)
@@ -21,11 +24,19 @@ defmodule Proxy do
 
   defp parse_upstream(upstream, mode) do
     {name, head} = List.pop_at(upstream, -1)
-    new_name = name
-      |> Atom.to_string()
-      |> Kernel.<>("_#{Atom.to_string(mode)}")
-      |> String.to_atom()
+    new_name = parse_name(name, mode)
 
     List.insert_at(head, -1, new_name)
+  end
+
+  defp parse_name([_, _, _, name], mode) do
+    parse_name(name, mode)
+  end
+
+  defp parse_name(name, mode) do
+    name
+    |> Atom.to_string()
+    |> Kernel.<>("_#{Atom.to_string(mode)}")
+    |> String.to_atom()
   end
 end
